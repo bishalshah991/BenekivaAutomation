45a376203fbed2e554df5d1a567efb69c245dc36
bishal991 <bishal@benekiva.com>
2023-05-10 06:18:04 +0545

    Implement Extent Report

---
+++ src/main/java/helper/ExtentReport/ExtentReportHelper.java
@@ -0,0 +1,23 @@
+package helper.ExtentReport;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+public class ExtentReportHelper {
+
+    static ExtentReports extentReports;
+
+    public static ExtentReports extentReportGenerator(){
+        String path=System.getProperty("user.dir")+"\\ExtentReportGenerate\\index.html";
+        ExtentSparkReporter reporter=new ExtentSparkReporter(path);
+        reporter.config().setReportName("UNITY Automation Result");
+        reporter.config().setDocumentTitle("Unity Test Result");
+        extentReports=new ExtentReports();
+        extentReports.attachReporter(reporter);
+        extentReports.setSystemInfo("QA","Bishal Shah");
+        extentReports.setSystemInfo("OS",System.getProperty("os.name"));
+        return  extentReports;
+    }
+
+
+}
